8b2b915bca75f9d150ea1d666d0869496271da74
kdo <otwmoo@gmail.com>
2016-01-31 19:22:51 +0900

    game board

--- sequenceList/src/SequenceList.scala
+++ sequenceList/src/SequenceList.scala
@@ -1,6 +1,6 @@
-val array: Array[String] = "15,8,2,4".split(",")
-
-var result = array collect {
-  case Array(x: String, y: String, _*) => (x.toInt, y.toInt)
-  case _ => (0, 0) // default
-}
+//val array: Array[String] = "15,8,2,4".split(",")
+//
+//var result = array collect {
+//  case Array(x: String, y: String, _*) => (x.toInt, y.toInt)
+//  case _ => (0, 0) // default
+//}
